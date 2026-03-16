8d1535f1ba32c69358dee2e54d521454850845ce
abafirst <abafirst.com@yahoo.com>
2026-03-16 14:45:41 -0700

    Fix: indent el = el.parentElement inside while loop in detectBackground function

--- public/js/resilience-compass.js
+++ public/js/resilience-compass.js
@@ -329,7 +329,7 @@ function detectBackground(canvas) {
         return calcLuminance(+m[1], +m[2], +m[3]) > 0.5;
       }
     }
-    el = el.parentElement;
+	    el = el.parentElement;
   }
     return false; // Default to dark
 
